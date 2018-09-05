1070f5da819560cb6068548c18555464e35977cb
Mike CK <mikeck93@gmail.com>
2018-09-05 11:10:44 +0300

    Get order by ID at /orders/<orderID> endpoint
    Return the order by searching for one that matches the ID. The route
    accepts a GET request.

--- fast/api.py
+++ fast/api.py
@@ -9,3 +9,13 @@ def all_orders ():
     session.clear()
     session['order_items'] = models.orders
     return json.dumps(session['order_items'])
+
+@app.route('/orders/<int:orderID>' , methods=['GET'])
+def single_order (orderID):
+    session.clear()
+    session['order_items'] = models.orders
+    for i in session['order_items']:
+        order = i.get(str('id'))
+        if order == orderID:
+            return json.dumps(i)
+    abort(404)
